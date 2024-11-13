db88ef5da2b48674723f5eef0ce9f662e43650ba
Rafał Majewski <goodheropl@gmail.com>
2024-11-13 16:49:29 +0100

    chore: Initialize the project

---
+++ .release-it.ts
@@ -0,0 +1,53 @@
+/** @type {import("release-it").Config} */
+const releaseItConfig = {
+	git: {
+		commitArgs: [],
+		commitMessage: "release: ${version}",
+		pushArgs: [],
+		tagArgs: [],
+	},
+	github: {
+		release: true,
+	},
+	hooks: {
+		"before:bump": "git switch -c 'release/${version}'",
+	},
+	npm: {
+		publish: false,
+	},
+	plugins: {
+		"@release-it/conventional-changelog": {
+			ignoreRecommendedBump: false,
+			preset: {
+				header: "# Changelog",
+				name: "conventionalcommits",
+				releaseCommitMessageFormat: "release: {{currentTag}}",
+				types: [
+					{
+						hidden: false,
+						section: "Features",
+						type: "feature",
+					},
+					{
+						hidden: false,
+						section: "Bug fixes",
+						type: "fix",
+					},
+					{
+						hidden: false,
+						section: "Chores",
+						type: "chore",
+					},
+					{
+						hidden: false,
+						section: "Releases",
+						type: "release",
+					},
+				],
+			},
+			strictSemVer: false,
+		},
+	},
+};
+
+export default releaseItConfig;
